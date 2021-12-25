08be12acbdfc478c78fcfd2e90bd3c3e111bbc25
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-25 12:29:13 +0100

    Created Transaction Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from '@nestjs/mongoose'
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/erp'),
+    TransactionModule
+  ],
   controllers: [],
   providers: [],
 })

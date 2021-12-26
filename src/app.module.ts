d36bcaf92b1459aec1d43c13d3dfd018f5bfad39
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-26 10:17:38 +0100

    created seller module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose'
 import { TransactionModule } from './transaction/transaction.module';
+import { SellerModule } from './seller/seller.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/erp'),
-    TransactionModule
+    TransactionModule,
+    SellerModule
   ],
   controllers: [],
   providers: [],

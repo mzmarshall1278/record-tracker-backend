99b13464dc83319e3a8a479106c1dd362b7bca65
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-28 15:06:47 +0100

    add filtering to get Transactions

--- src/transaction/Transaction.repository.ts
+++ src/transaction/Transaction.repository.ts
@@ -3,20 +3,62 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Transaction } from "./Transaction.model";
+import { GetTransactionFilterDto } from './dto/getTransactionFilter.dto';
 
 @Injectable()
 export class TransactionRepository {
     constructor(@InjectModel('Transaction') private readonly Transaction: Model<Transaction>){}
 
-    async getAllTransactions():Promise<Transaction[]>{
-        return this.Transaction.find();    
+    async getAllTransactions(getTransactionDto: GetTransactionFilterDto):Promise<Transaction[]>{
+        const {date, groupBy} = getTransactionDto;
+
+        const pipelines = [];
+
+        if (!Object.keys(getTransactionDto).length) {
+            pipelines.push({$group: {
+                _id: '$date',
+                 totalTransactions: {$sum: 1},
+                 totalSale: {$sum: '$price'},
+                 totalWeight: {$sum: '$weight'},
+                 totalQuantity: {$sum: '$quantity'},
+                //   details: {'$push':{
+                //       weight: '$weight',
+                //       quantity: '$quantity',
+                //       _id: '$_id',
+                //       price: '$price',
+                //       seller: '$seller'
+                //   }}
+                }})
+        }
+
+        if(date){
+            pipelines.push({$match: {date}})
+        }
+        // if(groupBy){
+        //     pipelines.push({
+        //         $group:{
+        //             _id: {
+        //                 $isoWeek:  new Date('date')
+        //             },
+        //             totalTransactions: {$sum: 1},
+        //          totalSale: {$sum: '$price'},
+        //          totalWeight: {$sum: '$weight'},
+        //          totalQuantity: {$sum: '$quantity'},
+        //         }
+        //     })
+        // }
+
+        pipelines.push(
+            {$sort: {_id: -1}}
+            )
+        return this.Transaction.aggregate(pipelines);    
     }
 
     async addTransaction(addTransactionDto: AddTransactionDto):Promise<Transaction>{
-        const {seller, price, weight, date, deal } = addTransactionDto;
+        const {seller, price, weight, date, deal, quantity } = addTransactionDto;
 
         const transaction = await new this.Transaction({
-            seller, price, weight, date, deal
+            seller, price, weight, date, deal, quantity
         }).save()
         return transaction;
     }

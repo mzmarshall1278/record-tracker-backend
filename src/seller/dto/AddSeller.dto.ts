535e4bd194a7ab055c5052fcb7d4d14f00f0a3d8
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-28 15:17:22 +0100

    imported seller status enum

--- src/seller/dto/AddSeller.dto.ts
+++ src/seller/dto/AddSeller.dto.ts
@@ -1,7 +1,10 @@
+import { SellerStatus } from "./getSellerfilter.dto";
+
 export class AddSellerDto {
     name: string;
     address: string;
     LGA: string;
     phone: number;
     deal: number;
+    status: SellerStatus;
 }

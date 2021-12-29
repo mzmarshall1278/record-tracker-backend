254660f88c1570207a96d703553ce43aab15639a
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-29 15:36:09 +0100

    Added date Joind field

--- src/seller/Seller.model.ts
+++ src/seller/Seller.model.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 import { SellerStatus } from './dto/getSellerfilter.dto';
-export const SellerSchema = new mongoose.Schema({
+ export const SellerSchema = new mongoose.Schema({
     name: {type: String, required: true},
     address: {type: String, required: true},
     LGA: {type: String, required: true},
@@ -8,7 +8,9 @@ export const SellerSchema = new mongoose.Schema({
     deal: {type: Number, required: true},
     status: {type: String, Enum:['COMPLETED' ,'PENDING']},
     transactions: {type: Array, required: true},
+    dateJoined: {type: Date, required: true}
 })
+SellerSchema.index({name: 'text', LGA: 'text', phone: 'text', })
 
 export interface Seller {
     id: string;
@@ -18,5 +20,6 @@ export interface Seller {
     phone: string;
     deal: number;
     status: SellerStatus;
+    dateJoined: Date;
     transactions: [];
 }

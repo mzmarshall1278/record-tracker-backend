b6a06adb4346fb306336526740df29ffe495c84e
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-30 14:45:04 +0100

    initialized auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose'
 import { TransactionModule } from './transaction/transaction.module';
 import { SellerModule } from './seller/seller.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/erp'),
     TransactionModule,
-    SellerModule
+    SellerModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],

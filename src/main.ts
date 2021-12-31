9deefc58b642bdfb1efce9ed328c79b8996be6a4
MZ Marshall <mzmarshall1278@gmail.com>
2021-12-31 23:59:27 +0100

    added cors and enviroment var

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  if(process.env.NODE_ENV === 'development') {
+    await app.listen(3000);
+  }
 }
 bootstrap();

7e08ebc19c889e47b5a3ee16ea8b702f202f474e
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-14 19:28:06 +0100

    injected authService and added login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { LoginDto } from './login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Get()
+    login(@Body() loginDto: LoginDto){
+        return this.authService.login(loginDto);
+    }
+
+}

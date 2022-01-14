d00bece557c239d1340c47e5fd1fcd0548769c97
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-14 19:29:53 +0100

    injected authRepository

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
+import { LoginDto } from './login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private authRepository: AuthRepository){}
+
+    async login(loginDto: LoginDto){
+        return this.authRepository.login(loginDto);
+    }
+}

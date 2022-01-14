5e99bc7dbd0ad63c4e8004abc54ca2561065f1c3
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-14 19:28:51 +0100

    injected authRepository and imported Userschema as model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
+import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
+import { UserSchema } from './User.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'User', schema: UserSchema}])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AuthRepository]
 })
 export class AuthModule {}

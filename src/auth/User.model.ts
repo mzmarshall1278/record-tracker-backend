b23443ede5af8b3ddaf85c384bc9232f8733b240
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-14 19:31:46 +0100

    added userRole enum and props

--- src/auth/User.model.ts
+++ src/auth/User.model.ts
@@ -1,10 +1,17 @@
 import * as mongoose from 'mongoose';
- export const SellerSchema = new mongoose.Schema({
+ export const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    role: {type: String, enum: ['SUPER_ADMIN', 'ADMIN'], required: true}
 })
 
-export interface Seller {
+export interface User {
     name: string;
     password: string;
+    role: UserRole;
+}
+
+export enum UserRole {
+    superAdmin = 'SUPER_ADMIN',
+    admin = 'ADMIN'
 }

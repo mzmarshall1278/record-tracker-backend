1745f662a9f94ba0282ac2e758951f15fdbf84d7
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-15 11:16:56 +0100

    imported Seller Module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -4,9 +4,10 @@ import { Module } from '@nestjs/common';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SellerModule } from 'src/seller/seller.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'Transaction', schema: TransactionSchema}])],
+  imports: [MongooseModule.forFeature([{name: 'Transaction', schema: TransactionSchema}]), SellerModule],
   controllers: [TransactionController],
   providers: [TransactionRepository, TransactionService]
 })

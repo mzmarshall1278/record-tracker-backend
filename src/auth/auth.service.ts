16cac356fc96608b90569e7cf882844a2ee21332
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-18 00:36:24 +0100

    add return types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,11 +6,11 @@ import { AuthDto } from './auth.dto';
 export class AuthService {
     constructor(private authRepository: AuthRepository){}
 
-    async login(loginDto: AuthDto){
+    async login(loginDto: AuthDto): Promise<{accessToken: string}>{
         return this.authRepository.login(loginDto);
     }
 
-    async signup(signupDto: AuthDto){
+    async signup(signupDto: AuthDto): Promise<string>{
         return this.authRepository.signup(signupDto);
     }
 }

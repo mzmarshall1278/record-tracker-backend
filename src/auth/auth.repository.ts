381b2e2519979fcb5080da10ac65ae055d604cfb
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-17 19:09:52 +0100

    add signup

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException } from '@nestjs/common';
 import { InjectModel } from "@nestjs/mongoose"
 import { Model } from "mongoose"
 import { User } from "./User.model"
@@ -7,7 +8,22 @@ export class AuthRepository {
     constructor(@InjectModel ('User') private readonly User: Model<User>){}
 
     async login(loginDto: AuthDto){
-        const {name, password} = loginDto;
+        const {username, password} = loginDto;
         return 'login'
+    }
+
+    async signup(signupDto: AuthDto){
+        const {username, password} = signupDto;
+
+        try {
+           const found = await this.User.findOne({ username });
+           if(found) {
+            throw new ConflictException('Username already taken');
+           }
+            await new this.User({username, password, role: 'ADMIN'}).save()
+            return 'success'
+        } catch (error) {
+            return error
+        }
     }
 }

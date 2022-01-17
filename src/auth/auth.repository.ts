7442af87058b34f9b425ea598ee8c66b194a0017
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-17 18:10:53 +0100

    fixed import in  repository

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,12 +1,12 @@
 import { InjectModel } from "@nestjs/mongoose"
 import { Model } from "mongoose"
 import { User } from "./User.model"
-import { LoginDto } from './login.dto';
+import { AuthDto } from './auth.dto';
 
 export class AuthRepository {
     constructor(@InjectModel ('User') private readonly User: Model<User>){}
 
-    async login(loginDto: LoginDto){
+    async login(loginDto: AuthDto){
         const {name, password} = loginDto;
         return 'login'
     }

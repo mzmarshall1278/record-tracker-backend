7de2f8f1bc5f6274d783cbac28ed56d80930bff8
MZ Marshall <mzmarshall1278@gmail.com>
2022-01-17 19:35:54 +0100

    add password hashing and generating salts

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -3,6 +3,7 @@ import { InjectModel } from "@nestjs/mongoose"
 import { Model } from "mongoose"
 import { User } from "./User.model"
 import { AuthDto } from './auth.dto';
+import * as bcrypt from 'bcrypt'
 
 export class AuthRepository {
     constructor(@InjectModel ('User') private readonly User: Model<User>){}
@@ -15,15 +16,23 @@ export class AuthRepository {
     async signup(signupDto: AuthDto){
         const {username, password} = signupDto;
 
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await this.hashPassword(password ,salt);
+
         try {
            const found = await this.User.findOne({ username });
            if(found) {
             throw new ConflictException('Username already taken');
            }
-            await new this.User({username, password, role: 'ADMIN'}).save()
-            return 'success'
+            await new this.User({username, password: hashedPassword, role: 'ADMIN', salt}).save()
+            return 'Account Created sucessfully'
         } catch (error) {
             return error
         }
     }
+
+    private async hashPassword (password: string, salt: string):Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+
 }
